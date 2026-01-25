c92bceb2869a3f25eb8080a17fc1a65308c5e97d
ozydevloper <ozydeveloper@gmail.com>
2026-01-26 03:44:06 +0700

    benerin tanggal +1,  week no edit

--- lib/getDatetime.ts
+++ lib/getDatetime.ts
@@ -1,6 +1,6 @@
 export const getHariIni = (thisWeek: boolean = false) => {
   const tanggal_hari_ini = new Date();
-  const tanggal_gt = String(tanggal_hari_ini.getDate()).padStart(2, "0");
+  const tanggal_gt = String(tanggal_hari_ini.getDate() + 1).padStart(2, "0");
   const bulan_gt = String(tanggal_hari_ini.getMonth() + 1).padStart(2, "0");
   const tahun_gt = tanggal_hari_ini.getFullYear();
 
@@ -19,7 +19,7 @@ export const getHariIni = (thisWeek: boolean = false) => {
     };
   }
   const tanggal_besok = new Date();
-  const tanggal_lt = String(tanggal_besok.getDate() + 1).padStart(2, "0");
+  const tanggal_lt = String(tanggal_besok.getDate() + 2).padStart(2, "0");
   const bulan_lt = String(tanggal_besok.getMonth() + 1).padStart(2, "0");
   const tahun_lt = tanggal_besok.getFullYear();
 
